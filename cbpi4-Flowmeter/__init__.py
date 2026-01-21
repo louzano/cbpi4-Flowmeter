807f83fe60a39e1c4535eec80bf917f91f274914
Edgar Louzano Verdile <68625105+louzano@users.noreply.github.com>
2026-01-21 15:50:44 -0300

    Update __init__.py

--- cbpi4-Flowmeter/__init__.py
+++ cbpi4-Flowmeter/__init__.py
@@ -24,10 +24,10 @@ logger = logging.getLogger(__name__)
 
 try:
     #import RPi.GPIO as GPIO
-    import LGPIO as GPIO
+    import rpi-lgpio as GPIO
     #import GPIOZERO as GPIO
-    #mode = GPIO.getmode()
-    #if (mode == None):
+    mode = GPIO.getmode()
+    if (mode == None):
  #ED alterado de GPIO para 21 na linha abaixo   
         GPIO.setmode(21)
 
