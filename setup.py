777e8af6ebf4790398eac93f9ac28810a540cef9
Edgar Louzano Verdile <68625105+louzano@users.noreply.github.com>
2026-01-21 12:04:44 -0300

    Atualização de autor

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='cbpi4-Flowmeter',
-      version='0.0.6',
-      description='CraftBeerPi4 Flowsensor / Step Plugin ',
-      author='Alexander Vollkopf',
-      author_email='avollkopf@web.de',
-      url='https://github.com/PiBrewing/cbpi4-Flowmeter',
+      version='0.0.7',
+      description='Sensor de fluxo e volume CBPI4 (YF-201)',
+      author='Edgar Louzano',
+      author_email='edgar.verdile@gmail.com',
+      url='https://github.com/louzano/cbpi4-Flowmeter',
       include_package_data=True,
       keywords='globalsettings',
       package_data={
